a51ba7086f935d55bf7bba33d50846c82f2d736e
João Oliveira <jvitoroliveira0711@gmail.com>
2024-08-18 15:36:05 -0300

    feature: Adiciona configuração basica

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,18 @@
 import androidx.compose.material.Scaffold
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import ui.DarkTheme
 import ui.components.Header
 import ui.components.TextEditor
 
 fun main() = application {
-    Window(title = "Plain Note", onCloseRequest = ::exitApplication) {
+    Window(
+        title = "Plain Note",
+        state = WindowState(width = 800.dp, height = 600.dp),
+        onCloseRequest = ::exitApplication
+    ) {
         DarkTheme {
             Scaffold(
                 topBar = {
